a8b20ccc034381a072c86c331c9ddd344ab19ce9
Litissia Hadjout <litissia.hadjout@gmail.com>
2025-08-05 01:09:49 +0100

    setup_frontend_init_axios

---
+++ src/app/auth-content/auth-content.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AxiosService } from '../axios.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-auth-content',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auth-content.component.html',
+  styleUrl: './auth-content.component.css'
+})
+export class AuthContentComponent implements OnInit{
+  data : string[] = [];
+
+  constructor(private axiosService: AxiosService){  }
+
+    ngOnInit() : void {
+      this.axiosService.request(
+        "GET",
+        "/messages",
+        null
+      ).then(
+        (response) => this.data = response.data
+      )
+    }
+
+}
