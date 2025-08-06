7c9af1450b070099eab9a9025b9ffe20125ac4f1
Litissia Hadjout <litissia.hadjout@gmail.com>
2025-08-06 01:50:00 +0100

    add_login_form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AuthContentComponent } from './auth-content/auth-content.component';
+import { ContentComponent } from './content/content.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AuthContentComponent],
+  imports: [RouterOutlet, HeaderComponent, AuthContentComponent, ContentComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/axios.service.ts
+++ src/app/axios.service.ts
@@ -8,6 +8,8 @@ export class AxiosService {
   constructor() {
     axios.defaults.baseURL = 'http://localhost:8080';
     axios.defaults.headers.post['Content-Type'] = 'application/json';
+    axios.defaults.withCredentials = true;//allow the backend to retrieve the header that contient authentification information
+
   }
   request(method: string, url: string, data: any): Promise<any> {
     return axios({
